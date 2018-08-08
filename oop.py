ea8ae305867ae6dbe0c2e11ca339021c45514af5
sekito charlse <bozicslxye1@gmail.com>
2018-08-08 14:31:05 +0200

    [Chore #159620614] add unitests for python class

--- oop.py
+++ oop.py
@@ -10,34 +10,23 @@ class SignUp(object):
         self.email_address = SignUp.validate_email(email_address)
         self.db = []
 
-    def user():
-        pass
-
     def combined_name(self):
         ''' Combine the first name and last name of the user '''
         self.name = self.first_name + self.last_name
-        # print(self.name)
         return self.name
 
     def submit(self):
         self.user = {'first_name': self.first_name,
                      'last_name': self.last_name, 'email': self.email_address}
         self.db.append(self.user)
-        print(self.db)
+        return self.db
 
     @staticmethod
     def validate_email(email_address):
         pattern = re.compile("^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$")
         if not pattern.match(email_address):
             print('Not a valid email')
-
             return False
         else:
             print('email is valid')
             return email_address
-
-
-signup = SignUp('chuck', 'mark','chuck1@mark.com')
-signup.combined_name()
-
-signup.submit()
